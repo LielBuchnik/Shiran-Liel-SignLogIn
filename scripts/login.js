15382ce857f4dd926e98dcc938206486a12e3434
Shiran <shirani1978@walla.com>
2023-11-05 19:11:41 +0200

    up

--- scripts/login.js
+++ scripts/login.js
@@ -14,7 +14,6 @@ function login(){
     else if (username.value==usersData.username && password.value==usersData.password)
     {
 
-                localStorage.setItem("userDetails", JSON.stringify(usersData));
                 window.location.href = "./homePage.html";   
     }
 }
